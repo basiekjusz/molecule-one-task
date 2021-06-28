e8a6745fc5ab4fb65db005bfc12dcb1df2383b37
basiekjusz <jasiekbusz@gmail.com>
2021-06-28 20:05:00 +0200

    Minor polish update

--- src/language/polish.ts
+++ src/language/polish.ts
@@ -5,10 +5,11 @@
 export const polish = {
   RUNIC_CONVERTER_TITLE: "Konwerter runiczny",
   RUNIC_CONVERTER_DESCRIPTION:
-    "Ten prosty konwerter runiczny został stworozny na potrzeby zadania rekrutacyjnego w Molecule.One. Pozwala on na konwersję liczby naturalnej ≤ 9999 podanej w systemie dziesiętnym na system runiczny.",
+    "Ten prosty konwerter runiczny został stworzony na potrzeby zadania rekrutacyjnego w Molecule.One. Pozwala on na konwersję liczby naturalnej ≤ 9999 podanej w systemie dziesiętnym na system runiczny.",
   VALUE_MUST_BE_NUMBER: "Podana wartość musi być liczbą.",
   VALUE_MUST_BE_NATURAL: "Podana liczba musi być naturalna.",
   VALUE_MUST_BE_SMALLER_THAN: "Podana liczba musi być mniejsza lub równa 9999.",
   CONVERTER_INPUT_LABEL: "Liczba do przekonwertowania",
   CONVERTER_SUBMIT_LABEL: "Konwertuj",
+  DOWNLOAD_LABEL: "Pobierz",
 };
