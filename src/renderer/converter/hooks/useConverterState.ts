4729013203a84cc09e2a946e74320bfd38457eb9
basiekjusz <jasiekbusz@gmail.com>
2021-06-28 20:41:28 +0200

    useConverterState implemenetation now generates runic representation on submit

--- src/renderer/converter/hooks/useConverterState.ts
+++ src/renderer/converter/hooks/useConverterState.ts
@@ -1,3 +1,53 @@
-function useConverterState() {}
+import { useState } from "react";
+import generateRunicRepresentation from "./generateRunicRepresentation";
+
+export interface ConverterState {
+  value: number;
+  image: string;
+  done: boolean;
+  loading: boolean;
+}
+
+const initialState: ConverterState = {
+  value: 0,
+  image: "",
+  done: false,
+  loading: false,
+};
+
+function useConverterState() {
+  const [state, setState] = useState<ConverterState>(initialState);
+
+  function setValue(value: number, stateCopy: ConverterState): void {
+    stateCopy.value = value;
+  }
+
+  function setImage(image: string, stateCopy: ConverterState): void {
+    stateCopy.image = image;
+  }
+
+  function setDone(done: boolean, stateCopy: ConverterState): void {
+    stateCopy.done = done;
+  }
+
+  function setLoading(loading: boolean, stateCopy: ConverterState): void {
+    stateCopy.loading = loading;
+  }
+
+  async function submit(value: number): Promise<void> {
+    let stateCopy: ConverterState = state;
+
+    const image = await generateRunicRepresentation(value);
+
+    setValue(value, stateCopy);
+    setImage(image, stateCopy);
+    setDone(true, stateCopy);
+    setLoading(false, stateCopy);
+
+    setState({ ...stateCopy });
+  }
+
+  return { state, submit };
+}
 
 export default useConverterState;
