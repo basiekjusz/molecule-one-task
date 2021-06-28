b11a07b18b82368c579f8ec6538efab59e9a811c
basiekjusz <jasiekbusz@gmail.com>
2021-06-28 15:05:14 +0200

    Fixed parsing number from string

--- src/renderer/converter/components/converterForm/hooks/validateConverterValue.ts
+++ src/renderer/converter/components/converterForm/hooks/validateConverterValue.ts
@@ -6,13 +6,13 @@ export interface ValidationOutput {
 }
 
 function validateConverterValue(value: string): ValidationOutput {
-  const valueNumber: number = parseInt(value);
+  const valueNumber: number = Number(value);
 
   let error: boolean = true;
   let errorText: string = "";
   if (isNaN(valueNumber)) {
     errorText = polish.VALUE_MUST_BE_NUMBER;
-  } else if (valueNumber % 1 !== 0 || valueNumber < 1) {
+  } else if (!Number.isInteger(valueNumber) || valueNumber < 1) {
     errorText = polish.VALUE_MUST_BE_NATURAL;
   } else if (valueNumber > 9999) {
     errorText = polish.VALUE_MUST_BE_SMALLER_THAN;
