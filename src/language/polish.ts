7dd2e3ff39f6abe4409ecadf53cb679596808191
basiekjusz <jasiekbusz@gmail.com>
2021-06-28 13:24:49 +0200

    Polish language file implementation

---
+++ src/language/polish.ts
@@ -0,0 +1,12 @@
+/**
+ * Stores all polish strings. Basic preparation for further multi-language support.
+ */
+
+export const polish = {
+  RUNIC_CONVERTER_TITLE: "Konwerter runiczny",
+  RUNIC_CONVERTER_DESCRIPTION:
+    "Ten prosty konwerter runiczny został stworozny na potrzeby zadania rekrutacyjnego w Molecule.One. Pozwala on na konwersję liczby naturalnej ≤ 9999 podanej w systemie dziesiętnym na system runiczny.",
+  VALUE_MUST_BE_NUMBER: "Podana wartość musi być liczbą.",
+  VALUE_MUST_BE_NATURAL: "Podana liczba musi być naturalna.",
+  VALUE_MUST_BE_SMALLER_THAN: "Podana liczba musi być mniejsza lub równa 9999.",
+};
