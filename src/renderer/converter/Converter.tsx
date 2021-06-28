1da1298f08707f037626ec265296fa2249c625fc
basiekjusz <jasiekbusz@gmail.com>
2021-06-28 13:27:07 +0200

    Basic Converter template implementation

--- src/renderer/converter/Converter.tsx
+++ src/renderer/converter/Converter.tsx
@@ -1,7 +1,24 @@
+import { Box, Typography } from "@material-ui/core";
 import React from "react";
+import { polish } from "../../language/polish";
+import ConverterForm from "./components/converterForm/ConverterForm";
+import ConverterOutput from "./components/ConverterOutput";
 
+/**
+ * Converter is a view responsible for converting decimal numbers to runic numeral system.
+ * Separated from App view due to possibility of further application development.
+ */
 function Converter() {
-  return <React.Fragment></React.Fragment>;
+  return (
+    <Box>
+      <Typography variant="h3">{polish.RUNIC_CONVERTER_TITLE}</Typography>
+      <Typography variant="subtitle1">
+        {polish.RUNIC_CONVERTER_DESCRIPTION}
+      </Typography>
+      <ConverterForm />
+      <ConverterOutput />
+    </Box>
+  );
 }
 
 export default Converter;
